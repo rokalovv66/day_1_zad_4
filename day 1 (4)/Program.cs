2f53a36037146c053bc4390e69b0b4290dfb969a
Нисик <@DESKTOP-ALORV4C>
2023-04-17 14:09:39 +0300

    Добавьте файлы проекта.

---
+++ day 1 (4)/Program.cs
@@ -0,0 +1,4 @@
+﻿int N = 12345; // замените на свое значение
+int secondsInHour = 60 * 60;
+int secondsSinceLastHour = N % secondsInHour;
+Console.WriteLine("Количество секунд, прошедших с начала последнего часа: " + secondsSinceLastHour);
